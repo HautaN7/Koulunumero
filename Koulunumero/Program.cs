e70ee53635468e48f0418781197354b4287ff118
Taavi Hautoniemi <taavihautoniemi@hotmail.com>
2016-01-13 14:27:37 +0200

    Koodi valmistettu.

--- Koulunumero/Program.cs
+++ Koulunumero/Program.cs
@@ -10,6 +10,21 @@ namespace Koulunumero
     {
         static void Main(string[] args)
         {
+
+            int pisteet;
+
+            Console.Write("Anna pisteet: ");
+            pisteet = int.Parse(Console.ReadLine());
+
+            if (pisteet >= 0 && pisteet <= 1) Console.WriteLine("Numero: 0");
+            if (pisteet >= 2 && pisteet <= 3) Console.WriteLine("Numero: 1");
+            if (pisteet >= 4 && pisteet <= 5) Console.WriteLine("Numero: 2");
+            if (pisteet >= 6 && pisteet <= 7) Console.WriteLine("Numero: 3");
+            if (pisteet >= 8 && pisteet <= 9) Console.WriteLine("Numero: 4");
+            if (pisteet >= 10 && pisteet <= 12) Console.WriteLine("Numero: 5");
+
+            Console.ReadLine();
+
         }
     }
 }
